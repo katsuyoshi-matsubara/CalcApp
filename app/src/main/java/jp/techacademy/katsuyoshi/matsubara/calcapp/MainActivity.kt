55f1aa04fece584f5179fe9ffb9c386f3b173592
katsuyoshi-matsubara <unikamonec@gmail.com>
2020-03-25 05:58:22 +0900

    Buttonタッチ時画面遷移機能を追加

--- app/src/main/java/jp/techacademy/katsuyoshi/matsubara/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/katsuyoshi/matsubara/calcapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package jp.techacademy.katsuyoshi.matsubara.calcapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+        val intent = Intent(this, CalcResult::class.java)
+        startActivity(intent)
+
+
     }
 }
