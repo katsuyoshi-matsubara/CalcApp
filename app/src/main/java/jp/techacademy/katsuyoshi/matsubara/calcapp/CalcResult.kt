ac3cf05c45a74bbd15fdfbcbed6abda048b7ecd3
katsuyoshi-matsubara <unikamonec@gmail.com>
2020-03-26 02:35:40 +0900

    四則計算結果を遷移後の画面に表示 例外処理・snackbarを追加

--- app/src/main/java/jp/techacademy/katsuyoshi/matsubara/calcapp/CalcResult.kt
+++ app/src/main/java/jp/techacademy/katsuyoshi/matsubara/calcapp/CalcResult.kt
@@ -2,11 +2,16 @@ package jp.techacademy.katsuyoshi.matsubara.calcapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calc_result.*
 
 class CalcResult : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calc_result)
+
+        val result = intent.getDoubleExtra("RESULT", 0.0)
+
+        textView.text = result.toString()
     }
 }
--- app/src/main/java/jp/techacademy/katsuyoshi/matsubara/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/katsuyoshi/matsubara/calcapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package jp.techacademy.katsuyoshi.matsubara.calcapp
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,10 +20,57 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button3.setOnClickListener(this)
         button4.setOnClickListener(this)
     }
+
     override fun onClick(v: View?) {
         val intent = Intent(this, CalcResult::class.java)
-        startActivity(intent)
+
+        val input1: Double
+        val input2: Double
+        try {
+            input1 = editText1.text.toString().toDouble()
+            input2 = editText2.text.toString().toDouble()
+
+            when (v?.id) {
+                R.id.button1 -> intent.putExtra("RESULT", input1 + input2)
+                R.id.button2 -> intent.putExtra("RESULT", input1 - input2)
+                R.id.button3 -> intent.putExtra("RESULT", input1 * input2)
+                R.id.button4 -> intent.putExtra("RESULT", input1 / input2)
+            }
 
 
+            startActivity(intent)
+
+        } catch (e: Exception) {
+            /* val s = "error"
+            when (v?.id) {
+                R.id.button1 -> button1.setOnClickListener { view -> */
+                    Snackbar.make(button1, "error", Snackbar.LENGTH_LONG)
+                        .setAction("Hide") {
+                        }.show()
+          /*      }
+                R.id.button2 -> button2.setOnClickListener { view ->
+                    Snackbar.make(view, s, Snackbar.LENGTH_LONG)
+                        .setAction("Hide") {
+                        }.show()
+                }
+                R.id.button3 -> button3.setOnClickListener { view ->
+                    Snackbar.make(view, s, Snackbar.LENGTH_LONG)
+                        .setAction("Hide") {
+                        }.show()
+                }
+                R.id.button4 -> button4.setOnClickListener { view ->
+                    Snackbar.make(view, s, Snackbar.LENGTH_LONG)
+                        .setAction("Hide") {
+                        }.show()
+                }
+            } */
+
+            println(e)
+        }
     }
+
 }
+
+
+
+
